d1716eb3194c68a4089c3947a6bf64d2fe49da04
renwei1029 <45345155+renwei1029@users.noreply.github.com>
2022-04-21 09:40:43 +0800

    Create wish.sol

---
+++ wish.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract Wish is Ownable{
+
+    // 在事件日志中存储wish 
+    function tryWish(string memory wish)  public onlyOwner{
+    }
+
+}
+// 使用的编译器版本为：0.8.0
